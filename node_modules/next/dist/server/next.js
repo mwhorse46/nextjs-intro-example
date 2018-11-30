2a69765536e04b83dcbccc45138e0a2f58f6df09
mwhorse46 <needmefordev@gmail.com>
2018-11-30 17:41:10 +0100

    delete node_module and .next

--- node_modules/next/dist/server/next.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// This file is used for when users run `require('next')`
-module.exports = function (opts) {
-  var Server = opts.dev ? require('./next-dev-server').default : require('./next-server').default;
-  return new Server(opts);
-};
